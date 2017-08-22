73b98d4e2c031fdd027113a3488f4753f97ac7a4
freeman507 <freeman.zanella@gmail.com>
2017-08-21 21:19:21 -0300

    first commit

---
+++ src/main/java/br/com/freeman/api/projeto/App.java
@@ -0,0 +1,13 @@
+package br.com.freeman.api.projeto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
