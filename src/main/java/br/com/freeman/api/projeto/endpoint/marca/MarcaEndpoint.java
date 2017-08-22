bf9c9e6ee9084be167cba486b96bfe6dbcbd9014
freeman507 <freeman.zanella@gmail.com>
2017-08-21 22:27:35 -0300

    preparando primeiro endpoint

--- src/main/java/br/com/freeman/api/projeto/endpoint/marca/MarcaEndpoint.java
+++ src/main/java/br/com/freeman/api/projeto/endpoint/marca/MarcaEndpoint.java
@@ -1,10 +1,21 @@
 package br.com.freeman.api.projeto.endpoint.marca;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.freeman.api.projeto.domain.marca.Marca;
+
 @RestController
+@RequestMapping(MarcaEndpoint.API_BASE_URI)
 public class MarcaEndpoint {
 
 	public static final String API_BASE_URI = "api/v1/marca";
+
+	@RequestMapping(value = "/{idMarca}", method = RequestMethod.GET)
+	public Marca getMarcaById(@PathVariable final Long idMarca) {
+		return marca;
+	}
 
 }
