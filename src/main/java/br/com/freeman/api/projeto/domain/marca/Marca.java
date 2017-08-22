5bd60a1a56ac5aa19b7ea959a0defa44dfecd42d
freeman507 <freeman.zanella@gmail.com>
2017-08-21 22:19:28 -0300

    criando rest controller

---
+++ src/main/java/br/com/freeman/api/projeto/domain/marca/Marca.java
@@ -0,0 +1,24 @@
+package br.com.freeman.api.projeto.domain.marca;
+
+public class Marca {
+
+	private Long idMarca;
+	private String dsMarca;
+
+	public Long getIdMarca() {
+		return idMarca;
+	}
+
+	public void setIdMarca(Long idMarca) {
+		this.idMarca = idMarca;
+	}
+
+	public String getDsMarca() {
+		return dsMarca;
+	}
+
+	public void setDsMarca(String dsMarca) {
+		this.dsMarca = dsMarca;
+	}
+
+}
---
+++ src/main/java/br/com/freeman/api/projeto/endpoint/marca/MarcaEndpoint.java
@@ -0,0 +1,10 @@
+package br.com.freeman.api.projeto.endpoint.marca;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MarcaEndpoint {
+
+	public static final String API_BASE_URI = "api/v1/marca";
+
+}
